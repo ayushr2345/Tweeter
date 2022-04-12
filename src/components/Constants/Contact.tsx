4b40217805d8c81e69ba280f32be29f0f74dfc4f
Ayush Ranjan <ayush.r2345@gmail.com>
2022-04-12 19:13:05 +0530

    working now

--- src/components/Constants/Contact.tsx
+++ src/components/Constants/Contact.tsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className="contact-section">
       <b>Contact me:</b>
       <div className="contact-email">Email: <a href="mailto:ayush.r2345@gmail.com">ayush.r2345@gmail.com</a></div>
-      <div className="contact-email">Phone: <a href="tel:+919198476520">+91 9198-476-520</a></div>
+      <div className="contact-email">Phone: <a href="tel:+9191984xxxxxx">+91 9198-xxx-xxx</a></div>
     </div>
   );
 }
