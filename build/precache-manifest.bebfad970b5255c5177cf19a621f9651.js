ace059064265362848dc099b729033c5105fb2f2
Ayush Ranjan <ayush.r2345@gmail.com>
2022-04-13 03:42:20 +0530

    buttons

--- build/precache-manifest.bebfad970b5255c5177cf19a621f9651.js
+++ build/precache-manifest.bebfad970b5255c5177cf19a621f9651.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/Tweeter/static/js/runtime~main.e8d7bb0f.js"
   },
   {
-    "revision": "b14ec16be60dd66532e0",
-    "url": "/Tweeter/static/js/main.30c8f11e.chunk.js"
+    "revision": "1587e732d4f2a337ca1d",
+    "url": "/Tweeter/static/js/main.b69d1b55.chunk.js"
   },
   {
     "revision": "3ec6ce584da2d558e2cb",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Tweeter/static/js/2.02845044.chunk.js"
   },
   {
-    "revision": "b14ec16be60dd66532e0",
+    "revision": "1587e732d4f2a337ca1d",
     "url": "/Tweeter/static/css/main.73603cbb.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Tweeter/static/css/2.76f79a34.chunk.css"
   },
   {
-    "revision": "de1de1977ac398aaac3c5405b2d73c76",
+    "revision": "ecfd9222f05657945b4003374b274aea",
     "url": "/Tweeter/index.html"
   }
 ];
