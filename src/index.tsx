420e14e242678f8a77651aa30fbe19331fc4a528
Ayush Ranjan <ayush.r2345@gmail.com>
2022-04-12 18:34:08 +0530

    react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
