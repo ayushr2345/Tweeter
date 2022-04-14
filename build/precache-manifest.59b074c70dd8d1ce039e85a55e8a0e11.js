8b7db9025b6cd75155d235025dbfd51db5ed19d4
Ayush Ranjan <ayush.r2345@gmail.com>
2022-04-14 22:29:04 +0530

    add resume

---
+++ build/precache-manifest.59b074c70dd8d1ce039e85a55e8a0e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "122a4f0718907323552f",
+    "url": "/Tweeter/static/js/runtime~main.e8d7bb0f.js"
+  },
+  {
+    "revision": "d3432031188958fb03ec",
+    "url": "/Tweeter/static/js/main.b3a7ab53.chunk.js"
+  },
+  {
+    "revision": "3ec6ce584da2d558e2cb",
+    "url": "/Tweeter/static/js/3.66b94e81.chunk.js"
+  },
+  {
+    "revision": "65120aa36cd0c85524ff",
+    "url": "/Tweeter/static/js/2.851b7ba8.chunk.js"
+  },
+  {
+    "revision": "d3432031188958fb03ec",
+    "url": "/Tweeter/static/css/main.ebb2fb3b.chunk.css"
+  },
+  {
+    "revision": "65120aa36cd0c85524ff",
+    "url": "/Tweeter/static/css/2.1a1fe533.chunk.css"
+  },
+  {
+    "revision": "9c742500c61bd50b24bd9d3872dcf73d",
+    "url": "/Tweeter/index.html"
+  }
+];
