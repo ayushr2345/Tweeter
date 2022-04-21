8149c111e70773e22c3c808d669cd39ba658ec17
Ayush Ranjan <ayush.r2345@gmail.com>
2022-04-21 19:14:08 +0530

    Updates

--- precache-manifest.0de024e39e41473eb0c49cc42fd6453b.js
+++ precache-manifest.0de024e39e41473eb0c49cc42fd6453b.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/Tweeter/static/js/runtime~main.e8d7bb0f.js"
   },
   {
-    "revision": "ee05ab8cf29d0b3cdbd9",
+    "revision": "3d9d1539a769fb1d3367",
     "url": "/Tweeter/static/js/main.b3a7ab53.chunk.js"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/Tweeter/static/js/2.851b7ba8.chunk.js"
   },
   {
-    "revision": "ee05ab8cf29d0b3cdbd9",
-    "url": "/Tweeter/static/css/main.e942706b.chunk.css"
+    "revision": "3d9d1539a769fb1d3367",
+    "url": "/Tweeter/static/css/main.aca02bee.chunk.css"
   },
   {
     "revision": "65120aa36cd0c85524ff",
     "url": "/Tweeter/static/css/2.1a1fe533.chunk.css"
   },
   {
-    "revision": "a78fb4a65a624f7b5e4a3b70b5a8b3bd",
+    "revision": "71e82ecd34a526a40ba272e198be5dbe",
     "url": "/Tweeter/index.html"
   }
 ];
